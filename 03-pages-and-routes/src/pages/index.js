1c8030fba7318aaba4d96efa5421310d0b420ef7
danielOuattara <daniel.ouattara@gmx.com>
2023-01-06 14:32:16 +0100

    pages & routes

---
+++ 03-pages-and-routes/src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export default function Home() {
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop& Deploy</h3>
+        <p>Ux designer & web developerbased in Combs la ville</p>
+      </div>
+    </section>
+  );
+}
