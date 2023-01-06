4883aad47f68149a219dd236cabad9f38d6026f0
danielOuattara <daniel.ouattara@gmx.com>
2023-01-06 14:32:29 +0100

    navbars & links

---
+++ 04-navbars-and-links/src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <section>
+      <Navbar />
+      <div>
+        <h2>Design</h2>
+        <h3>Develop& Deploy</h3>
+        <p>Ux designer & web developerbased in Combs la ville</p>
+      </div>
+    </section>
+  );
+}
---
+++ 04-navbars-and-links/src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      <h3>Porjects & Websites I've created</h3>
+    </div>
+  );
+}
