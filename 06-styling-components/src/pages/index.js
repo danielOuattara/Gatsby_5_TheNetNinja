86463384357f96f2a080ad7d1587eafe3e12a657
danielOuattara <daniel.ouattara@gmx.com>
2023-01-06 14:32:57 +0100

    styling components

---
+++ 06-styling-components/src/pages/index.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Layout from "./../components/Layout";
+import * as styles from "./../styles/home.module.css";
+import { Link } from "gatsby";
+export default function Home() {
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop& Deploy</h3>
+          <p>Ux designer & web developerbased in Combs la ville</p>
+          <Link className={styles.btn} to="/porfolio">
+            My portfolio project
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  );
+}
