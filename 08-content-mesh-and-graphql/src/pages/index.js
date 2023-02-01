fecf090f7cf2691bf768662c5ec446d1a2c66a84
danielOuattara <daniel.ouattara@gmx.com>
2023-02-02 00:56:29 +0100

    content mesh and graphql

---
+++ 08-content-mesh-and-graphql/src/pages/index.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Layout from "./../components/Layout";
+import * as styles from "./../styles/home.module.css";
+import { Link } from "gatsby";
+export default function Home() {
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop& Deploy</h3>
+          <p>Ux designer & web developerbased in Combs la ville</p>
+          <Link className={styles.btn} to="/projects">
+            My portfolio project
+          </Link>
+        </div>
+        <img src="/banner.png" alt="banner" style={{ maxWidth: "100%" }} />
+      </section>
+    </Layout>
+  );
+}
