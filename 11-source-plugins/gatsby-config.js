033471c057cff020cb47d061247a94ef424eadd3
danielOuattara <daniel.ouattara@gmx.com>
2023-02-06 22:21:52 +0100

    source plugins

---
+++ 11-source-plugins/gatsby-config.js
@@ -0,0 +1,35 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `notes`,
+        // Path to the directory
+        path: `${__dirname}/src/notes/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `projects`,
+        // Path to the directory
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: `Web warrior`,
+    description: `Web dev portfolio`,
+  },
+};
