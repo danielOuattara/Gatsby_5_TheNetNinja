c669b8d382e06381d1c2c31f9cb8491ddf00ad5d
danielOuattara <daniel.ouattara@gmx.com>
2023-02-06 22:23:09 +0100

    multiple queries

---
+++ 15-multiple-queries/src/pages/projects/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import * as styles from "./../../styles/project.module.css";
+import { graphql, Link } from "gatsby";
+
+export default function Projects(props) {
+  console.log(props);
+  const projects = props.data.projects.nodes;
+  const contact = props.data.contact.siteMetadata.contact;
+
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've created</h3>
+        <div className={styles.projects}>
+          {projects.map((project) => {
+            return (
+              <Link
+                to={`/projects/${project.frontmatter.slug}`}
+                key={project.id}
+              >
+                <div>
+                  <h3>{project.frontmatter.title}</h3>
+                  <p>{project.frontmatter.stack}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+        <p>Like what you see ? Email me at {contact} for a quote</p>
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query Projects {
+    projects: allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          slug
+          stack
+        }
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+`;
