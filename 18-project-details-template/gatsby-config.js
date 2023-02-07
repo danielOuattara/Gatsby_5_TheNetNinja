ece0c297073389e5dca574f3204d0297dbe66d78
danielOuattara <daniel.ouattara@gmx.com>
2023-02-07 23:49:48 +0100

    project details template

---
+++ 18-project-details-template/gatsby-config.js
@@ -0,0 +1,43 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `projects`,
+        // Path to the directory
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {},
+    },
+  ],
+  siteMetadata: {
+    title: `Web warrior`,
+    description: `Web dev portfolio`,
+    contact: "daniel.ouattara@gmx.com",
+  },
+};
---
+++ 18-project-details-template/src/pages/projects/index.js
@@ -0,0 +1,97 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import * as styles from "./../../styles/project.module.css";
+import { graphql, Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+export default function Projects(props) {
+  const projects = props.data.projects.nodes;
+  const contact = props.data.contact.siteMetadata.contact;
+
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've created</h3>
+
+        <div className={styles.projects}>
+          {projects.map((project) => {
+            return (
+              <Link
+                to={`/projects/${project.frontmatter.slug}`}
+                key={project.id}
+              >
+                <div>
+                  <GatsbyImage
+                    image={getImage(
+                      project.frontmatter.thumb.childImageSharp.gatsbyImageData,
+                    )}
+                    alt={project.frontmatter.slug}
+                  />
+                  <h3>{project.frontmatter.title}</h3>
+                  <p>{project.frontmatter.stack}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+
+        <p>Like what you see ? Email me at {contact} for a quote</p>
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query Projects {
+    projects: allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          slug
+          stack
+          thumb {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+`;
+
+// export const query = graphql`
+//   query Projects {
+//     projects: allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+//       nodes {
+//         id
+//         frontmatter {
+//           title
+//           slug
+//           stack
+//           thumb {
+//             childImageSharp {
+//               fluid {
+//                 src
+//                 srcSet
+//                 sizes
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//     contact: site {
+//       siteMetadata {
+//         contact
+//       }
+//     }
+//   }
+// `;
